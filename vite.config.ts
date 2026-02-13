a27572bbc2c16caeef76885aa75b915a4c184851
askorbin <61110079+zamaraevnikita@users.noreply.github.com>
2026-02-13 19:47:35 +0700

    menu

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    const isGhPages = process.env.GITHUB_PAGES === 'true';
     return {
+      base: isGhPages ? '/album_builder/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
